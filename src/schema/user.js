e86f7e7df24dc9ffddb7f839a1c3516cef1dd77b
kartikey <kartikkey0414@gmail.com>
2025-04-09 18:10:18 +0530

    Added basic user schema

---
+++ src/schema/user.js
@@ -0,0 +1,33 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5,
+        validate: {
+            validator: function (emailValue) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue);
+            },
+            message: 'Invalid email format'
+        },
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: 5
+    }
+}, {timestamps: true});
+
+const user = new mongoose.model("User", userSchema);
+
+export default user;
