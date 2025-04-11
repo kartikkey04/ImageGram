5039dfd0c13354c10b9cc3ebb64e9f5b74ee8e0f
kartikey <kartikkey0414@gmail.com>
2025-04-11 18:40:51 +0530

    Added aws config

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,3 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const DB_URI = process.env.DB_URI;
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_REGION = process.env.AWS_REGION;
